83d6371c5ad6f41f213309bef9501f278d74720e
Jesper Qvarfordt <jesperqvarfordt@gmail.com>
2017-09-19 17:31:44 +0200

    logging

--- routes/api/authentication.js
+++ routes/api/authentication.js
@@ -22,17 +22,20 @@ function serializeClient(req, res, next) {
   const newClient = new Client({
     user: req.user,
   });
-  Client.updateOrCreate({
-    user: req.user,
-  }, newClient, (err, client) => {
-    if (err) {
-      return res.status(400).send({
-        message: 'error saving client',
-      });
-    }
-    req.user.clientid = client.id;
-    return next();
-  });
+  Client.findOneAndUpdate(
+    { user: req.user }, // find a document with that filter
+    newClient, // document to insert when nothing was found
+    { upsert: true, new: true, runValidators: true }, // options
+    (err, client) => { // callback
+      if (err) {
+        return res.status(400).send({
+          message: 'error saving client',
+        });
+      }
+      req.user.clientid = client.id;
+      return next();
+    },
+  );
 }
 
 // ------- Token functions -------
